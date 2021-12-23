23be4207fc59ef944ecbc28bed12b7a470f8b15f
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2021-12-23 16:20:49 +0100

    extended required colors in the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     extend: {
       colors: {
         "dashboard_skyblue": "d6eef8",
+        "dashboard_purple": "#CCCCFF",
+        "dashboard_blue": "87cefa",
+        "dashboard_green": "#7FFFD4",
+        "dashboard_orange": "#FF5733"
       }
     },
   },
